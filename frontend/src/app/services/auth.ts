87a7f823a102bafe39abb207fd2bccf595ff6463
Ashen Lakmal <kahadawaarachchiashen@gmail.com>
2026-08-13 16:29:59 +0530

    feat: configure default routing and render login component

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Auth {}
+export class AuthService {
+
+  // Spring Boot API URL
+  private apiUrl = 'http://localhost:8080/api/v1/auth/login';
+
+  constructor(private http: HttpClient) { }
+
+  // Send HTTP POST request to backend
+  login(credentials: any): Observable<any> {
+    return this.http.post(this.apiUrl, credentials);
+  }
+}
